f5c9b9702d5058995729bb917aff51ba8590d179
Scott Brown <scottseeker@gmail.com>
2026-05-20 17:43:22 -0400

    fixup

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,6 +3,9 @@ import Header from '../Header/Header';
 import Sidebar from '../Sidebar/Sidebar';
 import styles from './Layout.module.css';
 
+const YEAR = new Date().getFullYear();
+const COPYRIGHT = `© 2025${YEAR > 2025 ? `\u2013${YEAR}` : ''} Async Labs Inc. All rights reserved.`;
+
 export default function Layout() {
   return (
     <div className={styles.layout}>
@@ -15,6 +18,7 @@ export default function Layout() {
           <Outlet />
         </main>
       </div>
+      <footer className={styles.footer}>{COPYRIGHT}</footer>
     </div>
   );
 }
